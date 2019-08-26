0e2bca88d8e5efca7270defb7f8edcd09a478529
bigwheelsmith <supercolin.dev@gmail.com>
2019-08-25 21:32:45 -0400

    added rook moves

--- js/valid_moves.js
+++ js/valid_moves.js
@@ -23,6 +23,7 @@ function piece_check(){
         case 'w_rook':
         case 'b_rook':
             console.log('this is a rook');
+            rook_moves()
             break;
     
         case 'w_knight': 
@@ -105,5 +106,61 @@ function pawn_moves() {
     // how to update the piece on the newly clicked square
     // console.log(move_selector.length);
     // add_valid_moves();
+
+}
+
+function rook_moves() {
+
+    let new_row = parseInt(selection.row);
+    let new_col = parseInt(selection.col);
+
+    console.log('starting with ' + new_row)
+    console.log('direction is ' + direction)
+
+
+    // if (direction == 'plus'){
+
+        //rows up
+        for (let i = selection.row; i <= ( 8 - selection.row ); i++) {
+            
+            move_selector[i] = $("[row="+ new_row +"][column="+ selection.col +"]");
+
+            new_row ++;
+        }
+        // rows down
+        for (let i = 1; i < selection.row; i++) {
+            
+            move_selector[(move_selector.length)] = $("[row="+ new_row +"][column="+ selection.col +"]");
+
+            new_row --;
+        }
+
+        // columns left
+        for (let i = selection.col; i <= ( 8 - selection.col ); i++) {
+            
+            move_selector[(move_selector.length)] = $("[row="+ selection.row +"][column="+ new_col +"]");
+
+            new_col ++;
+        }
+        // columns right
+        for (let i = selection.col; i <= selection.col; i++) {
+            
+            move_selector[(move_selector.length)] = $("[row="+ selection.row +"][column="+ new_col +"]");
+
+            new_col --;
+        }
+
+    // }else{
+
+        // rows down
+        for (let i = 1; i < selection.row; i++) {
+            
+            move_selector[i] = $("[row="+ new_row +"][column="+ selection.col +"]");
+
+            new_row --;
+        }
+    // }
+
+    move_selector[0] = $("[row="+ new_row +"][column="+ selection.col +"]");
 
 }
